15924944fc91ac75300801b980ed856ba48ad3b7
Taiki Noda <taiki.nd.real@gmail.com>
2021-07-25 15:56:49 +0900

    ready for RSpec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,10 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'ユーザー新規登録' do
-    
-    it 'nicknameが空ではない' do
+    it 'nicknameが空では登録できない' do
       # nicknameが空では登録できないテストコードを記述します
     end
-    it 'emailが空ではない' do
+    it 'emailが空では登録できない' do
       # emailが空では登録できないテストコードを記述します
     end
   end
